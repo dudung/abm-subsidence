99dcd355c3ddcbb41089e07f565b49de41be9096
Sparisoma Viridi <dudung@gmail.com>
2023-11-29 23:11:30 +0700

    update, pause

--- src/phase.js
+++ src/phase.js
@@ -5,10 +5,10 @@ class Phase {
 }
 
 Vacuum = Object.freeze(new Phase(0));
-Gas = Object.freeze(new Phase(1));
-Liquid = Object.freeze(new Phase(2));
-Solid = Object.freeze(new Phase(3));
-Granule = Object.freeze(new Phase(4));
-//Electrolye = Object.freeze(new Phase(5));
-//Plasma = Object.freeze(new Phase(6));
-Wall = Object.freeze(new Phase(-1));
+Gas = Object.freeze(new Phase(2));
+Liquid = Object.freeze(new Phase(3));
+Solid = Object.freeze(new Phase(4));
+Granule = Object.freeze(new Phase(5));
+//Electrolye = Object.freeze(new Phase(6));
+//Plasma = Object.freeze(new Phase(7));
+Wall = Object.freeze(new Phase(1));
--- src/space.js
+++ src/space.js
@@ -8,10 +8,28 @@ function createSpace(width, height) {
 }
 
 function addWallTo(space) {
-  space[0].fill(-1);
-  space.at(-1).fill(-1);
+  space[0].fill(Wall.type);
+  space.at(-1).fill(Wall.type);
   for(let i = 1; i < space.length - 1; i++) {
-    space[i][0] = -1;
-    space[i][space[i].length - 1] = -1;
+    space[i][0] = Wall.type;
+    space[i][space[i].length - 1] = Wall.type;
   }
+}
+
+function textFrom(space) {
+  let dig = 2;
+  let sep = ' ';
+  
+  let text = "";
+  let ROW = space.length;
+  let COL = space[0].length;
+  for(let i = 0; i < ROW; i++) {
+    let row = "";
+    for(let j = 0; j < COL; j++) {
+      row += String(space[i][j]).padStart(dig, 0);
+      if(j < COL - 1) row += sep;
+    }
+    text += row + '\n';
+  }
+  return text;
 }
